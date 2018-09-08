4010e8051f72098d324f8b0530fe786dc50fe7c5
jumper047 <jumper047@gmail.com>
2018-09-08 18:40:07 +0300

    add virtualkey code

--- app/vkeyboard.py
+++ app/vkeyboard.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QGridLayout, QPushButton, QSizePolicy, QWidget
+from PyQt5.QtCore import QEvent, Qt, pyqtSignal
+from PyQt5.QtWidgets import (QAction, QGridLayout, QMenu, QPushButton,
+                             QSizePolicy, QWidget)
 
 
 class VirtualKeyboard(QWidget):
@@ -27,8 +29,29 @@ class VirtualKeyboard(QWidget):
 
 class VirtualKey(QPushButton):
 
+    backlightSelected = pyqtSignal(str, str)
+
     def __init__(self, controller, number):
         super(VirtualKey, self).__init__(number)
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.installEventFilter(self)
 
-        # self.clicked.connect(controller.button_klicked)
+        self.backlightMenu = QMenu()
+        self.backlightMenu.addAction(QAction("Синий", self))
+        self.backlightMenu.addAction(QAction("Красный", self))
+        self.backlightMenu.addAction(QAction("Выкл.", self))
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.showMenu)
+
+    def showMenu(self, point):
+        action = self.backlightMenu.exec_(self.mapToGlobal(point)).text()
+        key = self.text()
+        self.backlightSelected.emit(key, action)
+
+    def eventFilter(self, obj, event):
+        if event.type() in (QEvent.MouseButtonPress, QEvent.MouseButtonDblClick)\
+                and event.button() == Qt.LeftButton:
+            print("Left key interrupted")
+            return True
+        else:
+            return super(VirtualKey, self).eventFilter(obj, event)
