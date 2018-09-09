6c5e6d011793bb601cb2f9639a06d0024672ad4f
jumper047 <jumper047@gmail.com>
2018-09-09 15:05:24 +0300

    add "no keyboard connected" window

--- app/vkeyboard.py
+++ app/vkeyboard.py
@@ -1,6 +1,9 @@
+import sys
+
 from PyQt5.QtCore import QEvent, Qt, pyqtSignal
-from PyQt5.QtWidgets import (QAction, QGridLayout, QMenu, QPushButton,
-                             QSizePolicy, QWidget)
+from PyQt5.QtWidgets import (QAction, QApplication, QButtonGroup, QGridLayout,
+                             QLabel, QMenu, QPushButton, QSizePolicy,
+                             QVBoxLayout, QWidget)
 
 
 class VirtualKeyboard(QWidget):
@@ -11,17 +14,18 @@ class VirtualKeyboard(QWidget):
 
     def initUi(self):
         self.setWindowTitle("Тест клавиатуры XK24")
-        self.createKeyboardLayout()
         self.setGeometry(400, 150, 200, 400)
-        self.show()
+        self.keyboard = dict()
+        self.createKeyboardLayout()
 
     def createKeyboardLayout(self):
         layout = QGridLayout()
         buttonNumber = 0
         for column in range(0, 4):
             for row in range(0, 6):
-                layout.addWidget(VirtualKey(
-                    self, str(buttonNumber)), row, column)
+                key = VirtualKey(self, str(buttonNumber))
+                self.keyboard[buttonNumber] = key
+                layout.addWidget(key, row, column)
                 buttonNumber += 1
             buttonNumber += 2
         self.setLayout(layout)
@@ -55,3 +59,27 @@ class VirtualKey(QPushButton):
             return True
         else:
             return super(VirtualKey, self).eventFilter(obj, event)
+
+
+class NoKeyboardWindow(QWidget):
+
+    def __init__(self):
+        super(NoKeyboardWindow, self).__init__()
+
+        layout = QVBoxLayout()
+        label = QLabel("Клавиатура не подключена")
+        button = QPushButton("Выход")
+        layout.addWidget(label)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+        self.setGeometry(400, 150, 100, 100)
+
+        button.clicked.connect(sys.exit)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = NoKeyboardWindow()
+    window.show()
+    sys.exit(app.exec_())
