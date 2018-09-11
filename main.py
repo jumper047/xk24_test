1d6317da8bc74f319f03f3e80a851b237a48615d
jumper047 <jumper047@gmail.com>
2018-09-11 15:28:31 +0300

    propper component's init

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from app.vkeyboard import VirtualKeyboard
+from app.controller import Controller
+from app.vkeyboard import NoKeyboardWindow, VirtualKeyboard
+from app.xk24 import XK24
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     vk = VirtualKeyboard()
+    nk = NoKeyboardWindow()
+    xk24 = XK24()
+    controller = Controller(vk, nk, xk24)
     sys.exit(app.exec_())
